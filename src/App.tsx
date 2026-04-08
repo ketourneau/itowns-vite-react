a359a59d3938b6f7029c7e692921fa3c011d4d11
Kevin ETOURNEAU <kevin.etourneau@sogelink.com>
2026-04-08 16:29:50 +0200

    Add potree2 sample

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,25 @@ import { useEffect } from "react";
 
 import "./App.css";
 
-import { View } from "itowns";
+import { PlanarControls, Potree2Layer, Potree2Source, View } from "itowns";
+import { Vector3 } from "three";
+
+function zoomToLayer(view: any, layer: any) {
+    const camera = view.camera3D;
+    const obb = layer.root.voxelOBB;
+    const center = obb.box3D.getCenter(new Vector3());
+    obb.localToWorld(center);
+    const length = obb.box3D.getSize(new Vector3()).length();
+    const fov = camera.fov * (Math.PI / 180);
+    const radius = length / 2;
+    const distance = radius / Math.tan(fov / 2);
+    camera.position.copy(center).addScaledVector(new Vector3(0, 0, 1), distance);
+    camera.up.copy(new Vector3(0, 1, 0));
+    camera.far = 2 * distance;
+    camera.lookAt(center);
+    camera.updateProjectionMatrix();
+    view.notifyChange(camera);
+}
 
 function App() {
     useEffect(() => {
@@ -10,8 +28,24 @@ function App() {
         if (viewerDiv === null) {
             return;
         }
+        const crs = "EPSG:4326";
         viewerDiv.style.display = "block";
-        const view = new View("EPSG:25831", viewerDiv);
+        const view = new View(crs, viewerDiv);
+        const potreeSource = new Potree2Source({
+            url: "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/master/pointclouds/potree2.0/lion/metadata.json",
+            crs
+        });
+        const potreeLayer = new Potree2Layer("Lion", {
+            source: potreeSource
+        });
+
+        function onLayerReady() {
+            zoomToLayer(view, potreeLayer);
+            const controls = new PlanarControls(view);
+            controls.groundLevel = potreeLayer.minElevationRange;
+        }
+
+        view.addLayer(potreeLayer).then(onLayerReady);
     }, []);
 
     return (
